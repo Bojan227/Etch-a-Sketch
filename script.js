9cd9974f3df3b80f70f101435fa6863ccf53893a
Bojan Blazevski <blazevski.bojan1@gmail.com>
2021-10-16 14:54:04 +0200

    Limit for the user input

--- script.js
+++ script.js
@@ -9,20 +9,21 @@ newGrid.addEventListener('click', makeGrid);
 function makeGrid(){
     let input = prompt('Enter your squares here?', '')
     let num = parseInt(input); 
-
-    
-
-    for(let i =0; i < num*num; i++){
-        let divs = document.createElement('div');
-        divs.classList.add('items');
-        
-    
-        divs.addEventListener('mouseover', function(e){
-                e.target.classList.add('myColor')  
-        })
-        container.appendChild(divs) 
-}
-        
+        if(num < 0 || num > 100 ){
+                alert("Please enter numbers between 0 and 100");
+        }else{
+                container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+                container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+            
+                for(let i =0; i < num*num; i++){
+                    let divs = document.createElement('div');
+                    divs.classList.add('items');
+                    divs.addEventListener('mouseover', function(e){
+                            e.target.classList.add('myColor')  
+                    })
+                    container.appendChild(divs) 
+            }
+        }      
 }
 clearButton.addEventListener('click', function(){
         container.innerHTML = ''
