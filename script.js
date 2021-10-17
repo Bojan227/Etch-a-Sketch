c0e71672aee4ce4abc6e6c08db5d1c89f6a06cea
Bojan Blazevski <blazevski.bojan1@gmail.com>
2021-10-17 14:03:31 +0200

    Added multiple buttons with different colours

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
 const container = document.querySelector('.container');
 const clearButton = document.querySelector('.clearButton');
 const newGrid = document.querySelector('.makeGrid');
+const randomButton = document.querySelector('.random');
+const redButton = document.querySelector('.red');
+const eraserButton = document.querySelector('.eraser')
+const blueButton = document.querySelector('.blue')
+const yellowButton = document.querySelector('.yellow')
 
-
-
-newGrid.addEventListener('click', makeGrid);
+function randomRgb(){
+        let x = Math.round(0xffffff * Math.random());
+        let r = x >> 16;
+        var g = x >> 8 & 255;
+        var b = x & 255;
+        return ('rgb(' + r + ', ' + g + ', ' + b + ')')
+}
 
 function makeGrid(){
     let input = prompt('Enter your squares here?', '')
     let num = parseInt(input); 
         if(num < 0 || num > 100 ){
-                alert("Please enter numbers between 0 and 100");
+                alert("Please enter number between 0 and 100");
         }else{
                 container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
                 container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-            
+                
                 for(let i =0; i < num*num; i++){
                     let divs = document.createElement('div');
                     divs.classList.add('items');
-                    divs.addEventListener('mouseover', function(e){
-                            divs.style.backgroundColor = 'red';
+
+                    redButton.addEventListener('click', function(){
+                            divs.addEventListener('mouseover', function(){
+                                    divs.style.backgroundColor = 'red';
+                            })
+                    })
+                    blueButton.addEventListener('click', function(){
+                        divs.addEventListener('mouseover', function(){
+                                divs.style.backgroundColor = 'blue';
+                        })
+                })
+                    yellowButton.addEventListener('click', function(){
+                        divs.addEventListener('mouseover', function(){
+                                divs.style.backgroundColor = 'yellow';
+                        })
+                })
+
+                    eraserButton.addEventListener('click', function(){
+                        divs.addEventListener('mouseover', function(){
+                                divs.style.backgroundColor = 'white';
+                        })
+                })
+
+                    randomButton.addEventListener('click', function(){
+                        divs.addEventListener('mouseover', function(){
+                        divs.style.backgroundColor = randomRgb();
+                    })
+                    
                     })
                     container.appendChild(divs) 
             }
         }      
 }
+
 clearButton.addEventListener('click', function(){
         container.innerHTML = ''
 });
 
-
+newGrid.addEventListener('click', makeGrid);
